a085f24633124c4c440bc52572ce0aa753f6b42a
jahidcx <muhdjahidhasan@gmail.com>
2024-12-26 01:13:23 +0600

    fifth problem have done

--- problem/src/problem_5.ts
+++ problem/src/problem_5.ts
@@ -2,6 +2,16 @@
     // problem 5
 
 
+    const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+
+        return obj[key];
+
+    }
+
+    // const person = { name: "Alice", age: 30, weight:65 };
+    // console.log(getPropertyValue(person, "weight"));
+
+
 
 
 
