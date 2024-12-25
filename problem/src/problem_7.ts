3d602a45a05a8a3aff0427f26a3558819f5d6615
jahidcx <muhdjahidhasan@gmail.com>
2024-12-26 01:56:45 +0600

    seventh problem have done

---
+++ problem/src/problem_7.ts
@@ -0,0 +1,46 @@
+{
+    // problem 7
+
+    class Car {
+        make: string;
+        model: string;
+        year: number
+
+        constructor(make: string, model: string, year: number) {
+            this.make = make;
+            this.model = model;
+            this.year = year
+        }
+
+        getCarAge() {
+            const currentYear = new Date().getFullYear();
+            const carAge = currentYear - this.year;
+            console.log(carAge);
+            
+        }
+    }
+
+
+    const car = new Car("Honda", "Civic", 2016);
+    car.getCarAge();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
